97d2720fc91a4c74e0354ac1b744f8c577a9f3ad
Max <m4xk96@gmail.com>
2023-05-16 21:42:58 +0200

    use named routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:gcx_device_manager/device_stream_publisher.dart';
+import 'package:gcx_device_manager/screens/device_detail/device_detail_view.dart';
+import 'package:gcx_device_manager/screens/device_detail/device_detail_view_model.dart';
 import 'package:gcx_device_manager/screens/device_list/device_list_viewmodel.dart';
 import 'package:gcx_device_manager/screens/device_list/device_list_view.dart';
 import 'package:gcx_device_manager/screens/add_device/add_device_view.dart';
@@ -22,69 +24,72 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'GCX device manager',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const MyHomePage(),
+        '/deviceListView': (context) =>
+            ChangeNotifierProvider<DeviceListViewModel>(
+              create: (_) => DeviceListViewModel(DeviceStreamPublisher()),
+              child: const DeviceListView(),
+            ),
+        '/deviceDetailScreen': (context) {
+          final String deviceId =
+              ModalRoute.of(context)!.settings.arguments as String;
+          return ChangeNotifierProvider<DeviceDetailViewModel>(
+            create: (_) =>
+                DeviceDetailViewModel(DeviceStreamPublisher(), deviceId),
+            child: DeviceDetailScreen(deviceId: deviceId),
+          );
+        },
+        '/addDeviceView': (context) =>
+            ChangeNotifierProvider<AddDeviceViewmodel>(
+              create: (_) => AddDeviceViewmodel(DeviceStreamPublisher()),
+              child: const AddDeviceView(),
+            ),
+        '/qrScannerView': (context) =>
+            ChangeNotifierProvider<QrScannerViewmodel>(
+              create: (_) => QrScannerViewmodel(),
+              child: const QrScannerScreen(),
+            ),
+        // Add more routes as needed
+      },
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(title),
+          title: const Text("GCX device manager"),
         ),
         body: Center(
           child: Column(
             children: [
               ElevatedButton(
                   onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                ChangeNotifierProvider<DeviceListViewModel>(
-                                  create: (_) => DeviceListViewModel(
-                                      DeviceStreamPublisher()),
-                                  child: const DeviceListView(),
-                                )));
+                    Navigator.pushNamed(context, '/deviceListView');
                   },
                   child: const Text("Go to Device List View")),
               ElevatedButton(
                   onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                ChangeNotifierProvider<AddDeviceViewmodel>(
-                                  create: (_) => AddDeviceViewmodel(
-                                      DeviceStreamPublisher()),
-                                  child: const AddDeviceView(),
-                                )));
+                    Navigator.pushNamed(context, '/addDeviceView');
                   },
                   child: const Text("Go to Add Device View")),
               ElevatedButton(
                   onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                ChangeNotifierProvider<QrScannerViewmodel>(
-                                  create: (_) => QrScannerViewmodel(),
-                                  child: QrScannerScreen(),
-                                )));
+                    Navigator.pushNamed(context, '/qrScannerView');
                   },
                   child: const Text("Go to Scanner")),
             ],
